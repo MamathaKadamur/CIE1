6b26ff9a5d8d45429c35bbe79778e8859f07fe7f
MamathaKadamur <mamatavk29@gmail.com>
2025-04-15 10:04:49 +0530

    created file

---
+++ SimpleIntrest.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner;
+
+public class SimpleIntrest{
+    public static void main(String[] args) {
+        float p,r,t,SIntrest;
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Enter the Principle");
+        p=scan.nextFloat();
+        System.out.println("Enter the Rate of Intrest");
+        r=scan.nextFloat();
+        System.out.println("Enter the Time Period");
+        t=scan.nextFloat();
+        scan.close();
+        SIntrest=(p*r*t)/100;
+        System.out.println("Simple Intrest Is:" + SIntrest);
+
+    }
+}
